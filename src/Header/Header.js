5fc6e0ea00f8ede419f59fdb194858728c7c1ccb
Rafiad <rafiadhoseen@gmail.com>
2022-10-29 19:14:47 +0600

    Header img & icon Done

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -100,8 +100,6 @@ const Header = () => {
             <div className="dropdown dropdown-end">
               <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                 <div className="w-10 rounded-full">
-                  {/* <img src={user?.photoURL} alt="" /> */}
-
                   <img src={user?.photoURL} alt="" />
                 </div>
               </label>
