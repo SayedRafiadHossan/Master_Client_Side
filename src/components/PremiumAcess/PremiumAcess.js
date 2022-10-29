af67fe2eebc71841dc8e4ba3ae0471a0937095af
Rafiad <rafiadhoseen@gmail.com>
2022-10-29 19:09:31 +0600

    Premium Card Done

--- src/components/PremiumAcess/PremiumAcess.js
+++ src/components/PremiumAcess/PremiumAcess.js
@@ -21,7 +21,7 @@ const PremiumAcess = () => {
           <img className="w-full" src={SingleCourse.img} alt="" />
         </div>
         <div className="lg:w-[25%]  items-center flex flex-col justify-center">
-          <h1 className="mb-4 text-xl font-bold">{SingleCourse.title}</h1>
+          <h1 className="mb-4 text-xl font-extrabold">{SingleCourse.title}</h1>
           <p className="mb-4 font-bold">ID : {SingleCourse.id}</p>
           <p className="font-bold">Price : {SingleCourse.price}</p>
         </div>
